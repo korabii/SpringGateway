cc801a06646067132e4a11f72ce2ec065ed0301b
Korab Beqiraj <korabbeqiraj@Korabs-MBP.broadband>
2024-01-25 20:40:52 +0000

    first commit

---
+++ src/test/java/com/korbii/demo/SpringCloudGatewayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korbii.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
